4e2e9d508fc6793f340c98743dd9bd37aefd6c27
voxeljorz <voxel.jorz.xssi@gmail.com>
2023-06-30 12:23:39 +0800

    Add files via upload

---
+++ kill-notepad.ps1
@@ -0,0 +1,10 @@
+﻿do{
+    
+    $running = Get-Process notepad -ErrorAction SilentlyContinue
+    if ($running)
+    {
+        Stop-Process -name notepad
+    }
+    
+Start-Sleep -s 1
+} while (1 -eq 1)
